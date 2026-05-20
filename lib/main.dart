7d6ba25b67d7d6b3e9d6feff50f0636f842b6a98
Elshaarawy hassan <elshaarawyhassan7@gmail.com>
2026-05-20 11:18:16 +0300

    change name app

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ModoevaApp());
+}
+
+class ModoevaApp extends StatelessWidget {
+  const ModoevaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, 
+      
+    );
+  }
+}
